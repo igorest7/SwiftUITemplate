dc54104d76d3cafc0716927eef816e9e98bb5a37
Igor Nakonetsnoi <igor.nakonetsnoi@gmail.com>
2024-04-15 21:29:35 +0100

    Initial Commit

---
+++ SwiftUITemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITemplate
+//
+//  Created by Igor Nakonetsnoi on 15/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITemplate/SwiftUITemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITemplateApp.swift
+//  SwiftUITemplate
+//
+//  Created by Igor Nakonetsnoi on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
